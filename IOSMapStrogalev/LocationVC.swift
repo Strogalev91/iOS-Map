27329dbdcd0690c735efcc989a153d2b182c942c
Strogalev Ilia <74052206+Strogalev91@users.noreply.github.com>
2020-11-24 13:07:41 +0300

    iOSMap

---
+++ IOSMapStrogalev/LocationVC.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  IOSMapStrogalev
+//
+//  Created by Strogalev Ilia on 23/11/2020.
+//
+
+import UIKit
+import CoreLocation
+
+class LocationVC: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+
+    @IBOutlet weak var dataLat: UILabel!
+    @IBOutlet weak var dataLon: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        checkAuthorization()
+        // Do any additional setup after loading the view.
+    }
+    
+    func checkAuthorization() {
+        
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+            print("Error")
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        default:
+            break
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last?.coordinate {
+            dataLat.text = String(location.latitude)
+            dataLon.text = String(location.longitude)
+            locationManager.stopUpdatingLocation()
+        }
+    }
+
+
+}
+
